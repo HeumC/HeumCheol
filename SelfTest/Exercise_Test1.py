db357e5e34a1db27324831753d0a5cb9d50ee44f
HeumCheol <739336377@qq.com>
2020-08-24 12:08:19 +0900

    调整引用情况

--- SelfTest/Exercise_Test1.py
+++ SelfTest/Exercise_Test1.py
@@ -1,4 +1,4 @@
-from HCheol import HCheol_Exercise as FBHS
+import HCheol_Exercise as HCER
 
 # FBHS.Exercise_Human('韩鑫哲', 190, 104.5, 21, FBHS.Exercise_Enum.SEX_MALE, FBHS.Exercise_Enum.MIS_LEVEL_3,
 #                     FBHS.Exercise_Enum.MORPHY_MESOMORPHY, FBHS.Exercise_Enum.TARGET_REDUCE_FAT).Information()
@@ -12,8 +12,8 @@ from HCheol import HCheol_Exercise as FBHS
 #                     FBHS.Exercise_Enum.MORPHY_ECTOMORPHY, FBHS.Exercise_Enum.TARGET_REDUCE_FAT).Information()
 # print()
 
-FBHS.Exercise_Human('欧阳根明', 167, 52, 22, FBHS.Exercise_Enum.SEX_MALE, FBHS.Exercise_Enum.MIS_LEVEL_1,
-                    FBHS.Exercise_Enum.MORPHY_ECTOMORPHY, FBHS.Exercise_Enum.TARGET_INCREASE_MUSCLE).Information()
+HCER.Exercise_Human('欧阳根明', 167, 52, 22, HCER.Exercise_Enum.SEX_MALE, HCER.Exercise_Enum.MIS_LEVEL_1,
+                    HCER.Exercise_Enum.MORPHY_ECTOMORPHY, HCER.Exercise_Enum.TARGET_INCREASE_MUSCLE).Information()
 print()
 
 # FBHS.Exercise_Human('姚明', 226, 140.6, 40, FBHS.Exercise_Enum.SEX_MALE, FBHS.Exercise_Enum.MIS_LEVEL_3,
